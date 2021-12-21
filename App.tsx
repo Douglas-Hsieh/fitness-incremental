0b0c1f9360116f8feddcbb49268dbc26d7b3b819
Douglas Hsieh <douglas.w.hsieh@gmail.com>
2021-12-20 19:23:19 -0800

    Record steps today and steps today since last visit

--- App.tsx
+++ App.tsx
@@ -10,37 +10,40 @@ import { GameState, INITIAL_GAME_STATE } from './assets/data/GameState';
 import Screen from './src/enums/Screen';
 import { UnlocksScreen } from './src/screens/UnlocksScreen';
 import { PrestigeScreen } from './src/screens/PrestigeScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const GOOGLE_FIT_AUTHORIZATION_OPTIONS = {
+  scopes: [
+    Scopes.FITNESS_ACTIVITY_READ,
+    Scopes.FITNESS_ACTIVITY_WRITE,
+    Scopes.FITNESS_BODY_READ,
+    Scopes.FITNESS_BODY_WRITE,
+  ],
+}
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
 
   const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
-  const [steps, setSteps] = useState<number>();
 
   const [screen, setScreen] = useState<Screen>(Screen.WelcomeBack);
   const [gameState, setGameState] = useState<GameState>(INITIAL_GAME_STATE);
+  const [stepsTodayLastVisit, setStepsTodayLastVisit] = useState<number>(0);
+  const [stepsToday, setStepsToday] = useState<number>(0)
 
 
   useEffect(() => {
     EStyleSheet.build();
 
     GoogleFit.checkIsAuthorized().then(() => {
-      console.log(GoogleFit.isAuthorized) // Then you can simply refer to `GoogleFit.isAuthorized` boolean.
+      console.log(GoogleFit.isAuthorized)
       setIsAuthorized(GoogleFit.isAuthorized);
     })
   }, [])
 
   useEffect(() => {
     if (!GoogleFit.isAuthorized) {
-      const options = {
-        scopes: [
-          Scopes.FITNESS_ACTIVITY_READ,
-          Scopes.FITNESS_ACTIVITY_WRITE,
-          Scopes.FITNESS_BODY_READ,
-          Scopes.FITNESS_BODY_WRITE,
-        ],
-      }
-      GoogleFit.authorize(options)
+      GoogleFit.authorize(GOOGLE_FIT_AUTHORIZATION_OPTIONS)
         .then(authResult => {
           if (authResult.success) {
             console.log("AUTH_SUCCESS");
@@ -53,16 +56,35 @@ export default function App() {
           console.log("AUTH_ERROR");
         })
     } else {
-      const date = new Date();
-      GoogleFit.getDailySteps(date)
-        .then(stepsResponseList => {
-          const stepsResponse = stepsResponseList.find(stepsResponse => stepsResponse.source === 'com.google.android.gms:estimated_steps');
-          if (stepsResponse) {
-            console.log('Steps taken today: ', stepsResponse.steps[0].value);
-            setSteps(stepsResponse.steps[0].value);
-          }
-        })
-        .catch(error => console.log(error))
+
+      // Client authorized to use Google Fit
+      const getAndSet = async () => {
+
+        const getAndSetStepsTodayLastVisit = async () => {
+          const stepsTodayLastVisitString = await AsyncStorage.getItem('stepsToday')
+          const stepsTodayLastVisit = stepsTodayLastVisitString ? JSON.parse(stepsTodayLastVisitString) as number : 0
+          setStepsTodayLastVisit(stepsTodayLastVisit)
+          console.log('stepsTodayLastVisit', stepsTodayLastVisit)
+        }
+        await getAndSetStepsTodayLastVisit()
+  
+        const getAndSetStepsToday = async () => {
+          const date = new Date();
+          GoogleFit.getDailySteps(date)
+            .then(stepsResponseList => {
+              const stepsResponse = stepsResponseList.find(stepsResponse => stepsResponse.source === 'com.google.android.gms:estimated_steps');
+              if (stepsResponse && stepsResponse.steps.length > 0) {
+                const stepsToday = stepsResponse.steps[0].value
+                setStepsToday(stepsToday)
+                console.log('stepsToday', stepsToday);
+                AsyncStorage.setItem('stepsToday', JSON.stringify(stepsToday))
+              }
+            })
+            .catch(error => console.log(error))
+        }
+        await getAndSetStepsToday()
+      }
+      getAndSet()
     }
   }, [isAuthorized])
 
@@ -76,6 +98,8 @@ export default function App() {
       return (
         <WelcomeBackScreen
           setScreen={setScreen}
+          stepsTodayLastVisit={stepsTodayLastVisit}
+          stepsToday={stepsToday}
         />
       )
     case Screen.Home:
