c41ce427affe4ff0c3be2cf87a710f4f4110ff5f
Douglas Hsieh <douglas.w.hsieh@gmail.com>
2022-01-14 20:49:13 -0800

    Add styling customizable

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,34 +1,35 @@
-import { GestureResponderEvent, Pressable, Text } from "react-native"
+import { GestureResponderEvent, Pressable, StyleProp, Text, TextStyle, ViewStyle } from "react-native"
 import EStyleSheet from "react-native-extended-stylesheet"
 import colors from "../../assets/colors/colors"
 
 interface ButtonProps {
   text: string;
   onPress?: (((event: GestureResponderEvent) => void) & (() => void)) | undefined
-  color?: string;
+  style?: StyleProp<ViewStyle>;
+  textStyle?: StyleProp<TextStyle>
 }
 
-export const Button = ({text, onPress, color = colors.orange3}: ButtonProps) => {
-
-  const styles = EStyleSheet.create({
-    button: {
-      backgroundColor: color,
-      padding: 10,
-      borderRadius: 10,
-    },
-    text: {
-      color: colors.white,
-      fontFamily: 'oleo-script',
-      fontSize: '1.5rem',
-    },
-  })
+export const Button = ({text, onPress, style, textStyle}: ButtonProps) => {
 
   return (
     <Pressable
-      style={({pressed}) => [{opacity: pressed ? 0.5 : 1.0}, styles.button]}
+      style={({pressed}) => [{opacity: pressed ? 0.5 : 1.0}, styles.button, style]}
       onPress={onPress}>
-      <Text style={styles.text}>{text}</Text>
+      <Text style={[styles.text, textStyle]}>{text}</Text>
     </Pressable>
   )
 }
 
+const styles = EStyleSheet.create({
+  button: {
+    backgroundColor: colors.orange3,
+    padding: 10,
+    borderRadius: 10,
+  },
+  text: {
+    color: colors.white,
+    fontFamily: 'oleo-script',
+    fontSize: '1.5rem',
+    textAlign: 'center',
+  },
+})
