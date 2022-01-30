34205b7809238873714064a82473eac69df8e75b
Douglas Hsieh <douglas.w.hsieh@gmail.com>
2022-01-29 16:26:45 -0800

    Add push notifications

---
+++ src/push-notifications.ts
@@ -0,0 +1,33 @@
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+import { Platform } from "react-native";
+
+export const registerForPushNotificationsAsync = async () => {
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+  
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    const experienceId = '@random_permutation/fitness-incremental'
+    const token = (await Notifications.getExpoPushTokenAsync({ experienceId: experienceId })).data;
+    console.log('token.data', token);
+    return token;
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+};
